3f763fcb7a3527a916a86f1c7fe753e2f7f9827d
sangvu060498-coder <sangvu060498@gmail.com>
2025-12-25 22:00:01 +0700

    Update load_data.py

--- src/data_loading/load_data.py
+++ src/data_loading/load_data.py
@@ -1,5 +1,23 @@
 def load_all_data():
-    #load csv
-    đặt tên: customers, orders, order_items, products, payments
+    customers = pd.read_csv(path + 'customers.csv')
+    orders = pd.read_csv(path + 'orders.csv')
+    order_items = pd.read_csv(path + 'order_items.csv')
+    products = pd.read_csv(path + 'products.csv')
+    payments = pd.read_csv(path + 'order_payments.csv')
+    
+    return customers, orders, order_items, products, payments
 def explore(df, name):
-    # head, info,. describe
+    print(f"\n==================== BẢNG: {name.upper()} ====================")
+   
+    print(df.head())
+    
+    print(" Thông tin cấu trúc (info):")
+    df.info() 
+    
+    print(" Thống kê mô tả (describe):")
+    print(df.describe())
+    
+    print(" Kiểm tra giá trị thiếu (isnull):")
+    missing = df.isnull().sum()
+    print(missing[missing > 0] if missing.sum() > 0 else "Dữ liệu đầy đủ, không thiếu.")
+    print("-" * 60)
